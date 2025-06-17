970430b35fb99af507111601adaa765f811dc7e9
ChChandan <chandanvja@gmail.com>
2025-06-17 15:11:06 +1000

    added function to retrieve current running processes and log them into the logfile.txt

--- WinMonitorAgent/WinMonitorAgent.cpp
+++ WinMonitorAgent/WinMonitorAgent.cpp
@@ -8,6 +8,41 @@
 #include <iostream>
 using namespace std;
 
+
+void getRunningProcess() {
+	DWORD processes[1024], cbNeeded, processCount;
+    unsigned int i;
+
+    if(!EnumProcesses(processes, sizeof(processes), &cbNeeded)) {
+        std::cerr << "Failed to enumerate processes." << std::endl;
+        return;
+	}
+
+	processCount = cbNeeded / sizeof(DWORD);
+    std::ofstream logFile("C:\\Users\\chandan\\source\\repos\\WinMonitorAgent\\logfile.txt", std::ios::app);
+    
+
+    for (i = 0; i < processCount; i++) {
+		if (processes[i] == 0) continue; // Skip if process ID is 0
+        TCHAR processName[MAX_PATH] = TEXT("<unknown>");
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processes[i]);
+        if (hProcess) {
+            HMODULE hmod;
+            DWORD cbNeededMod;
+            if (EnumProcessModules(hProcess, &hmod, sizeof(hmod), &cbNeededMod)) {
+                GetModuleBaseName(hProcess, hmod, processName, sizeof(processName) / sizeof(TCHAR));
+
+            }
+
+            logFile << processName << " (PID: " << processes[i] << ")\n";
+            CloseHandle(hProcess);
+
+        }
+    
+    }
+        logFile.close();
+}
+
 void getRam() {
 	MEMORYSTATUSEX memoryStatus;
 	memoryStatus.dwLength = sizeof(MEMORYSTATUSEX);
@@ -31,7 +66,8 @@ void getRam() {
 
 int main()
 {
-	getRam();
+    //getRam();
+	getRunningProcess();
 	
     return 0;
 }
